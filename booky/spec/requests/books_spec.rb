e7f794de8494d2b4b514e76167a4337d29793193
salamidrus <idrussalam95@gmail.com>
2021-06-03 03:06:36 +0700

    Unit Test for Destroy Endpoint

--- booky/spec/requests/books_spec.rb
+++ booky/spec/requests/books_spec.rb
@@ -1,12 +1,37 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-    it 'returns all the books' do
-        FactoryBot.create(:book, title: '1995', author: 'George')
-        FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G Wells')
-        get '/api/v1/books'
+    describe 'GET /books' do
+        before do
+            FactoryBot.create(:book, title: '1995', author: 'George')
+            FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G Wells')    
+        end
 
-        expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body).size).to eq(2)
+        it 'returns all the books' do
+            get '/api/v1/books'
+
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(2)
+        end
+    end
+
+    describe 'POST /books' do
+        it 'create a new book' do
+            expect {
+                post '/api/v1/books', params: {book: {title: 'Javascript for dummy', author: 'Elthon'} }
+            }.to change { Book.count }.from(0).to(1)
+        
+            expect(response).to have_http_status(:created)
+        end
+    end
+
+    describe 'DELETE /books/:id' do
+        let!(:book) { FactoryBot.create(:book, title: '1995', author: 'George') }
+        
+        it 'deletes a book' do
+            expect {delete "/api/v1/books/#{book.id}"}.to change {Book.count}.from(1).to(0)
+
+            expect(response).to have_http_status(:no_content)
+        end
     end
 end
