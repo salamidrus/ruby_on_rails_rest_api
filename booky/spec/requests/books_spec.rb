a6990cfdc0b53f1e370477c97710fe262f7b1658
salamidrus <idrussalam95@gmail.com>
2021-06-04 11:22:12 +0700

    Add API Response Body Specs and Spec Helpers

--- booky/spec/requests/books_spec.rb
+++ booky/spec/requests/books_spec.rb
@@ -1,17 +1,38 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
+    let(:first_author) {FactoryBot.create(:author, first_name: 'George', last_name: 'Orwell', age: 15)}
+    let(:second_author) {FactoryBot.create(:author, first_name: 'H.G', last_name: 'Wells', age: 15)}
+    
     describe 'GET /books' do
+        
+
         before do
-            FactoryBot.create(:book, title: '1995', author: 'George')
-            FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G Wells')    
+            FactoryBot.create(:book, title: '1995', author: first_author)
+            FactoryBot.create(:book, title: 'The Time Machine', author: second_author)    
         end
 
         it 'returns all the books' do
             get '/api/v1/books'
 
             expect(response).to have_http_status(:success)
-            expect(JSON.parse(response.body).size).to eq(2)
+            expect(response_body.size).to eq(2)
+            expect(response_body).to eq(
+                [
+                    {
+                        'id' => 1,
+                        'title' => '1995',
+                        'author_name' => 'George Orwell',
+                        'author_age'=> 15 
+                    },
+                    {
+                        'id' => 2,
+                        'title' => 'The Time Machine',
+                        'author_name' => 'H.G Wells',
+                        'author_age'=> 15 
+                    }
+                ]
+            )
         end
     end
 
@@ -26,11 +47,17 @@ describe 'Books API', type: :request do
         
             expect(response).to have_http_status(:created)
             expect(Author.count).to eq(1)
+            expect(response_body).to eq({
+                'id' => 1,
+                'title' => 'Javascript for dummy',
+                'author_name' => 'Andy Weir',
+                'author_age'=> 15 
+               })
         end
     end
 
     describe 'DELETE /books/:id' do
-        let!(:book) { FactoryBot.create(:book, title: '1995', author: 'George') }
+        let!(:book) { FactoryBot.create(:book, title: '1995', author: first_author) }
         
         it 'deletes a book' do
             expect {delete "/api/v1/books/#{book.id}"}.to change {Book.count}.from(1).to(0)
