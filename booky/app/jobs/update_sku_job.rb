966d3b007ecac5d2fa9d97bb879bda76b9a3117b
salamidrus <idrussalam95@gmail.com>
2021-06-06 11:23:38 +0700

    Testing the Active Job

--- booky/app/jobs/update_sku_job.rb
+++ booky/app/jobs/update_sku_job.rb
@@ -1,3 +1,4 @@
+# To update SKU in another server
 class UpdateSkuJob < ApplicationJob
   queue_as :default
 
@@ -5,7 +6,7 @@ class UpdateSkuJob < ApplicationJob
     uri = URI('http://localhost:4567/update_sku')
     req = Net::HTTP::Post.new(uri, 'Content-Type' => 'applciation/json')
     req.body = { sku: '123', name: book_name }.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
   end
--- booky/spec/controllers/books_controller_spec.rb
+++ booky/spec/controllers/books_controller_spec.rb
@@ -1,9 +1,23 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::BooksController, type: :controller do
-  it 'has a max limit of 100' do
-    expect(Book).to receive(:limit).with(100).and_call_original
+  describe 'GET index' do
+    it 'has a max limit of 100' do
+      expect(Book).to receive(:limit).with(100).and_call_original
 
-    get :index, params: { limit: 999 }
+      get :index, params: { limit: 999 }
+    end
+  end
+
+  describe 'POST create' do
+    let(:book_name) { 'Algorithm' }
+    it 'calls UpdateSKUJob with correct params' do
+      expect(UpdateSkuJob).to receive(:perform_later).with(book_name)
+
+      post :create, params: {
+        author: { first_name: 'JK', last_name: 'Rowling', age: 48 },
+        book: { title: book_name }
+      }
+    end
   end
 end
