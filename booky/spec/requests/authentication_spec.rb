f58172b7d543640542abe77350ef18165f259bbc
salamidrus <idrussalam95@gmail.com>
2021-06-06 16:17:22 +0700

    Add Test for Auth Route

---
+++ booky/spec/requests/authentication_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Authentication', type: :request do
+  describe 'POST /authenticate' do
+    it 'authenticates the client' do
+      post '/api/v1/authenticate', params: { username: 'BookSeller99', password: "Password1" }
+
+      expect(response).to have_http_status(:created)
+      expect(response_body).to eq({ 'token' => '123' })
+    end
+
+    it 'returns error when username is missing' do
+      post '/api/v1/authenticate', params: { password: "Password1" }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response_body).to eq({ "error" => "param is missing or the value is empty: username\nDid you mean?  password\n               controller\n               action" })
+    end
+
+    it 'returns error when password is missing' do
+      post '/api/v1/authenticate', params: { username: 'BookSeller99' }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response_body).to eq({ "error" => "param is missing or the value is empty: password\nDid you mean?  action\n               username\n               controller" })
+    end
+  end
+end
